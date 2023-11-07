bf80df66246dbb8dfef08b7dfdf2ac774fbfcd7a
Pedro Valério <pedroddjkK@gmail.com>
2023-11-07 15:04:07 -0300

    Chamada de API e Estilização

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,54 @@
 <script lang="ts">
-  let count = 0;
+  import type { RickAndMortyCharacter } from "$lib/types";
+  import { onMount } from "svelte";
+
+  let data: RickAndMortyCharacter[] | undefined;
+
+  onMount(async () => {
+    const res = await fetch("https://rickandmortyapi.com/api/character");
+    const body = await res.json();
+    data = body.results;
+  });
 </script>
 
-<h1>{count}</h1>
-<button on:click={() => count++} >Aumentar</button>
-<button on:click={() => count--}>Diminuir</button>
+<div class="wrapper">
+  {#if data}
+    {#each data as character}
+      <div>
+        <h1>{character.name}</h1>
+        <h3>{character.gender}</h3>
+
+        <img src={character.image} alt={character.name} />
+
+        {#if character.gender === "Male"}
+          <h3>Olá</h3>
+        {/if}
+      </div>
+    {/each}
+  {/if}
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+
+  .wrapper div {
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px;
+    width: 400px;
+    height: 600px;
+  }
+
+  .wrapper div img {
+    width: 100%;
+    height: auto;
+  }
+  
+</style>
