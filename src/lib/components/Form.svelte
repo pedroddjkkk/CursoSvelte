e5298d40308cc97f5d41eede311fffb414e8ed30
Pedro Valério <pedroddjkK@gmail.com>
2023-11-07 16:57:21 -0300

    Formulário front-end

---
+++ src/lib/components/Form.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+</script>
+
+<form action="">
+  <h3>Criar Post</h3>
+  <div>
+    <label for="title">Titulo</label>
+    <input type="text" name="title" id="title" />
+    <label for="content">Conteúdo</label>
+    <textarea name="content" id="content" cols="30" rows="10" />
+  </div>
+  <button type="submit">Enviar</button>
+</form>
+
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    margin: 45px auto 0 auto;
+  }
+
+  form > div {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  input {
+    height: 30px;
+  }
+
+  textarea {
+    resize: vertical;
+  }
+
+  button {
+    border: none;
+    margin-top: 20px;
+    background-color: rgb(8, 182, 8);
+    padding: 10px;
+    color: #fff;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
-<script lang="ts">
-  let count = 0;
+<script>
+  import Form from "$lib/components/Form.svelte";
 </script>
 
-<h1>{count}</h1>
-<button on:click={() => count++} >Aumentar</button>
-<button on:click={() => count--}>Diminuir</button>
+<Form />
